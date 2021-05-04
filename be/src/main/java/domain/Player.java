c1a1faa39ecfda2b49a4dbd23977f5deff6322f1
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-05-04 16:26:11 +0900

    feat: ✨Entity 추가
    Player와 Team Entity 추가

---
+++ be/src/main/java/domain/Player.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.springframework.data.annotation.Id;
+
+public class Player {
+
+    @Id
+    private Long id;
+    private String name;
+
+    private Player() {
+    }
+
+    public Player(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
