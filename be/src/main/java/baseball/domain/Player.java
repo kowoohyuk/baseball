615826b0abeeac7796f2cd7812cc7eea6320d444
95degree <95degree@naver.com>
2021-05-06 14:51:33 +0900

    feat : player에 필드 변수 추가 #8
    db에 추가적으로 들어간 column들을 추가해 주기 위해 필드변수를 추가했습니다.

--- be/src/main/java/baseball/domain/Player.java
+++ be/src/main/java/baseball/domain/Player.java
@@ -1,12 +1,17 @@
 package baseball.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 public class Player {
 
     @Id
     private Long id;
     private String name;
+    private String role;
+
+    @Column(value = "plate_status")
+    private boolean status;
 
     private Player() {
     }
@@ -23,4 +28,13 @@ public class Player {
     public String getName() {
         return name;
     }
+
+    public String getRole() {
+        return role;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
 }
